4032746519205c2e2ac2951235f091e20894d738
Hugo O <hugortega_ipn@hotmail.com>
2017-01-20 01:06:09 -0600

    Clase Generica.java

---
+++ Generic.java
@@ -0,0 +1,22 @@
+
+public class Generic<T>{
+	
+	private T primero;
+
+	public Generic()}{
+		primero = null;
+	}
+
+	public void setPrimero(T nuevoValor){
+		
+		primero = nuevoValor;
+
+	}
+
+	public T getPrimero(){
+		
+		return primero;
+
+	}
+
+}
